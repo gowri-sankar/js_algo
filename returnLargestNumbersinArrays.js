81a7e9e8723ba4602ba120192f70f60543812e3a
Gowri <ivcheero@gmail.com>
2017-10-05 22:23:32 +0530

    Update returnLargestNumbersinArrays.js

--- returnLargestNumbersinArrays.js
+++ returnLargestNumbersinArrays.js
@@ -1,5 +1,6 @@
 //Return an array consisting of the largest number from each provided sub-array. For simplicity,
 //the provided array will contain exactly 4 sub-arrays. [[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]
+
 function largestOfFour(arr) {
   // You can do this!
   var list = [];
