78d58f3bea99b4aff4e26785f58d48cdf3001d49
Gowri <ivcheero@gmail.com>
2017-10-05 22:22:08 +0530

    Create returnLargestNumbersinArrays.js

---
+++ returnLargestNumbersinArrays.js
@@ -0,0 +1,17 @@
+
+function largestOfFour(arr) {
+  // You can do this!
+  var list = [];
+  for(var i=0; i<arr.length;i++){
+    var longest=0;
+    for(j=0; j<arr[i].length;j++){
+      if(arr[i][j]>longest){
+        longest = arr[i][j];
+      }
+    }
+    list.push(longest);
+  }
+  return list;
+}
+
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
