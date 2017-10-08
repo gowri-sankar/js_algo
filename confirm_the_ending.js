566daa78bcee5e6fc326964d061c59e36281a65b
Gowri <ivcheero@gmail.com>
2017-10-08 20:25:03 +0530

    Create confirm_the_ending.js

---
+++ confirm_the_ending.js
@@ -0,0 +1,14 @@
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  if(str.substr(str.length-target.length) == target){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+confirmEnding("Bastian", "n");
