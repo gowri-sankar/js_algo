fe044daa74805815c69ea5006709493d027df9fd
Gowri <ivcheero@gmail.com>
2017-10-09 15:49:32 +0530

    Create where_it_belongs.js

---
+++ where_it_belongs.js
@@ -0,0 +1,14 @@
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  arr.push(num);
+  arr.sort(function(a,b){
+    return a - b;
+  });
+  
+  return arr.indexOf(num);
+}
+
+getIndexToIns([40, 60], 50);
