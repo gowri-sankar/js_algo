ce7ed2ae2e71c0e61efeffd1e30f443de1bd3d08
Gowri <ivcheero@gmail.com>
2017-10-11 18:58:06 +0530

    revised

--- [easy]slasher_flick.js
+++ [easy]slasher_flick.js
@@ -1,7 +1,6 @@
 // Return the remaining elements of an array after chopping off n elements from the head.
 
 function slasher(arr, howMany) {
-  // it doesn't always pay to be first
   for(var i=0;i<howMany;i++){
     arr.shift();
   }
